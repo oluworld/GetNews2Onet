4b730c0b53b69cff9e04cfe83152f65d9a2c7c8d
oluworld <oluoluolu@protonmail.com>
2021-10-09 23:59:13 -0400

    fix paths in script

--- print5.sh
+++ print5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-find ./GetNews2Onet -type f | grep .py$ > list
-cat list | xargs sha1sum >> GetNews2Onet-`date +%b%d-%H%M`.txt
-echo =========*=========*=========*=========*=========*=========*=========*=========* >> GetNews2Onet-`date +%b%d-%H%M`.txt
-cat list | xargs pr -n -f -l 84 >> GetNews2Onet-`date +%b%d-%H%M`.txt
+find ../GetNews2Onet -type f | grep .py$ > list
+cat list | xargs sha1sum >> ../GetNews2Onet-`date +%b%d-%H%M`.txt
+echo =========*=========*=========*=========*=========*=========*=========*=========* >> ../GetNews2Onet-`date +%b%d-%H%M`.txt
+cat list | xargs pr -n -f -l 84 >> ../GetNews2Onet-`date +%b%d-%H%M`.txt
 rm list
 
