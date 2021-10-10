6b9cd31f2a1d51d76c7087d189829c5e5810a572
oluworld <oluoluolu@protonmail.com>
2021-10-09 23:06:50 -0400

    refactor: created netapps.GetNews2Onet.GetNews2Onet

--- GetNews2Onet.py
+++ GetNews2Onet.py
@@ -1,66 +1,9 @@
-import os, sys, time
-from etoffiutils import dumptextfile, true
-from AppWorks.Invocation.OnetAddNewsgroupMessage import Onet_EmptyMessage, OANM1_JOB_EXECUTOR
+import os, sys
 from etoffiutils2.OpMeasure import OpMeasure
-from netapps.GetNews2Onet.MailExtractors import MailExtractNull, MailExtractEML, MailExtractCBracket, WrongFormat
+from netapps.GetNews2Onet.GetNews2Onet import GetNews2Onet
 
 
 class pException(Exception): pass
-
-
-class MailExtractMultiplexer:
-	def __init__(self):
-		self.executor = OANM1_JOB_EXECUTOR(None)
-		self._eml = MailExtractEML(self.executor)
-		self._cbr = MailExtractCBracket(self.executor)
-		self._nul = MailExtractNull(self.executor)
-	
-	def get(self, filename):
-		extension = filename[-4:].lower()
-		if extension == '.eml':
-			return self._eml
-		elif extension == '.{c}':
-			return self._cbr
-		else:
-			return self._nul
-
-
-class GetNews2Onet:
-	def __init__(self):
-		self.multiplexer = MailExtractMultiplexer()
-	
-	def do_file(self, dirname, filename):
-		fullname = '%s/%s' % (dirname, filename)
-		try:
-			ll = dumptextfile(fullname, true)
-			self.translate_message(ll, dirname, filename)
-			os.unlink(fullname)
-		except WrongFormat, e:
-			print '** wrong format'
-		except Onet_EmptyMessage, e:
-			#			print e
-			print '** Message is empty'
-		except IOError, e:
-			print '== Error Decoding', filename
-			print '\t', e
-			print '================='
-	
-	def translate_message(self, ll, dn, filename):
-		extractor = self.multiplexer.get(filename)
-		res = extractor.extract_srv_info(ll, dn, filename)
-		if res is None:
-			return
-		server, group, msgnum, f1, f2 = res
-		
-		# generate spectionary
-		spectionary = {}
-		spectionary['server'] = server
-		spectionary['group_name'] = group
-		spectionary['msgnum'] = msgnum
-		spectionary['options'] = 'save-headers extract-contents-in-shared delete-orig'
-		
-		# invoke message adding component
-		extractor.add_newsgroup_message(time.ctime(time.time()), spectionary, f1, f2)
 
 
 #class NullMessage(Exception):
---
+++ netapps/GetNews2Onet/GetNews2Onet.py
@@ -0,0 +1,61 @@
+import os
+import time
+
+from AppWorks.Invocation.OnetAddNewsgroupMessage import OANM1_JOB_EXECUTOR, Onet_EmptyMessage
+from etoffiutils import dumptextfile, true
+from netapps.GetNews2Onet.MailExtractors import MailExtractEML, MailExtractCBracket, MailExtractNull, WrongFormat
+
+
+class MailExtractMultiplexer:
+	def __init__(self):
+		self.executor = OANM1_JOB_EXECUTOR(None)
+		self._eml = MailExtractEML(self.executor)
+		self._cbr = MailExtractCBracket(self.executor)
+		self._nul = MailExtractNull(self.executor)
+	
+	def get(self, filename):
+		extension = filename[-4:].lower()
+		if extension == '.eml':
+			return self._eml
+		elif extension == '.{c}':
+			return self._cbr
+		else:
+			return self._nul
+
+
+class GetNews2Onet:
+	def __init__(self):
+		self.multiplexer = MailExtractMultiplexer()
+	
+	def do_file(self, dirname, filename):
+		fullname = '%s/%s' % (dirname, filename)
+		try:
+			ll = dumptextfile(fullname, true)
+			self.translate_message(ll, dirname, filename)
+			os.unlink(fullname)
+		except WrongFormat, e:
+			print '** wrong format'
+		except Onet_EmptyMessage, e:
+			#			print e
+			print '** Message is empty'
+		except IOError, e:
+			print '== Error Decoding', filename
+			print '\t', e
+			print '================='
+	
+	def translate_message(self, ll, dn, filename):
+		extractor = self.multiplexer.get(filename)
+		res = extractor.extract_srv_info(ll, dn, filename)
+		if res is None:
+			return
+		server, group, msgnum, f1, f2 = res
+		
+		# generate spectionary
+		spectionary = {}
+		spectionary['server'] = server
+		spectionary['group_name'] = group
+		spectionary['msgnum'] = msgnum
+		spectionary['options'] = 'save-headers extract-contents-in-shared delete-orig'
+		
+		# invoke message adding component
+		extractor.add_newsgroup_message(time.ctime(time.time()), spectionary, f1, f2)
